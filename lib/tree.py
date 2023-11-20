6604d346d80178bf8c028d64c05fa7ebe501ca6c
BookmDan <danoh329@gmail.com>
2023-11-19 17:59:07 -0800

    pass

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,19 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._get_element_by_id_helper(self.root, id)
+
+  def _get_element_by_id_helper(self, node, target_id):
+    if node is None:
+        return None
+
+    if node.get('id') == target_id:
+        return node
+
+    for child in node.get('children', []):
+        result = self._get_element_by_id_helper(child, target_id)
+        if result:
+            return result
+
+    return None
+
